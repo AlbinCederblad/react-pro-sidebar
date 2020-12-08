7e82e2266583baa3c243465b399334b354beca1f
Albin Cederblad <albin.cederblad@hotmail.com>
2020-12-08 13:25:24 +0100

    added dist

---
+++ dist/ProSidebar/Menu/MenuItem.d.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+export interface Props {
+    children?: React.ReactNode;
+    className?: string;
+    icon?: React.ReactNode;
+    active?: boolean;
+    prefix?: React.ReactNode;
+    suffix?: React.ReactNode;
+    firstchild?: boolean;
+    popperarrow?: boolean;
+    id?: string;
+}
+declare const _default: React.ForwardRefExoticComponent<Props & React.RefAttributes<unknown>>;
+export default _default;
